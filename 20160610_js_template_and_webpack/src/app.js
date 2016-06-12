1b82e6b7ec5a2fe3ec8b4b0ba4904a2491f2fa4f
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2016-06-12 10:46:34 +0900

    [Webpack1 template] add ejs runner

--- 20160610_js_template_and_webpack/src/app.js
+++ 20160610_js_template_and_webpack/src/app.js
@@ -1,1 +1,7 @@
-console.log("hello world")
+console.log("Hello, Template!")
+
+const fs = require('fs');
+const ejs = require('ejs');
+const templateString = fs.readFileSync('src/template.ejs', 'utf-8');
+html = ejs.render(templateString, {people: ['geddy', 'neil', 'alex']});
+console.log(html)
