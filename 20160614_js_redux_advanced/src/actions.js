2052291e41137876c85d8edfd43445f105f59893
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2016-06-14 13:59:01 +0900

    [Redux Advanced] Implemente wrapper of async action

--- 20160614_js_redux_advanced/src/actions.js
+++ 20160614_js_redux_advanced/src/actions.js
@@ -30,40 +30,35 @@ export const receivePosts = (subreddit, json) => {
   }
 }
 
-// Meet our first thunk action creator!
-// Though its insides are different, you would use it just like any other action creator:
-// store.dispatch(fetchPosts('reactjs'))
-
-export const fetchPosts = (subreddit) => {
-
-  // Thunk middleware knows how to handle functions.
-  // It passes the dispatch method as an argument to the function,
-  // thus making it able to dispatch actions itself.
-
+const fetchPosts = (subreddit) => {
   return (dispatch) => {
-
-    // First dispatch: the app state is updated to inform
-    // that the API call is starting.
-
     dispatch(requestPosts(subreddit))
-
-    // The function called by the thunk middleware can return a value,
-    // that is passed on as the return value of the dispatch method.
-
-    // In this case, we return a promise to wait for.
-    // This is not required by thunk middleware, but it is convenient for us.
-
     return fetch(`http://www.reddit.com/r/${subreddit}.json`)
       .then(response => response.json())
-      .then(json =>
+      .then(json => dispatch(receivePosts(subreddit, json)))
+  }
+}
 
-        // We can dispatch many times!
-        // Here, we update the app state with the results of the API call.
+const shouldFetchPosts = (state, subreddit) => {
+  const posts = state.postsBySubreddit[subreddit]
+  if (!posts) {
+    return true
+  } else if (posts.isFetching) {
+    return false
+  } else {
+    return posts.didInvalidate
+  }
+}
 
-        dispatch(receivePosts(subreddit, json))
-      )
+export const fetchPostsIfNeeded = (subreddit) => {
 
-      // In a real world app, you also want to
-      // catch any error in the network call.
+  return (dispatch, getState) => {
+    if (shouldFetchPosts(getState(), subreddit)) {
+      // Dispatch a thunk from thunk!
+      return dispatch(fetchPosts(subreddit))
+    } else {
+      // Let the calling code know there's nothing to wait for.
+      return Promise.resolve()
+    }
   }
 }
--- 20160614_js_redux_advanced/src/index.js
+++ 20160614_js_redux_advanced/src/index.js
@@ -14,40 +14,11 @@ const store = createStore(
   )
 )
 
-import { selectSubreddit, fetchPosts } from './actions'
+import { selectSubreddit, fetchPostsIfNeeded } from './actions'
 store.dispatch(selectSubreddit('redux'))
 console.log(store.getState())
 store.dispatch(selectSubreddit('reactjs'))
-store.dispatch(fetchPosts('reactjs')).then(() => {
-  console.log("Finished fetch")
+store.dispatch(fetchPostsIfNeeded('reactjs')).then(() => {
+  console.log("Finished Fetch posts")
   console.log(store.getState())
 })
-
-// const d = {
-//   data:{
-//     children: [{data:"c"}]
-//   }
-// }
-// store.dispatch(recievePosts('redux', d))
-// console.log(store.getState())
-
-// import thunkMiddleware from 'redux-thunk'
-// import createLogger from 'redux-logger'
-// import { createStore, applyMiddleware } from 'redux'
-// import { selectSubreddit, fetchPosts } from './actions'
-// import rootReducer from './reducers'
-//
-// const loggerMiddleware = createLogger()
-//
-// const store = createStore(
-//   rootReducer,
-//   applyMiddleware(
-//     thunkMiddleware, // lets us dispatch() functions
-//     loggerMiddleware // neat middleware that logs actions
-//   )
-// )
-//
-// store.dispatch(selectSubreddit('reactjs'))
-// store.dispatch(fetchPosts('reactjs')).then(() =>
-//   console.log(store.getState())
-// )
