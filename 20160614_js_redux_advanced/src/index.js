39cfb6890023b3805404384cd461ed418ecf7812
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2016-06-14 13:49:18 +0900

    [Redux Advanced] Implemente async action

--- 20160614_js_redux_advanced/src/index.js
+++ 20160614_js_redux_advanced/src/index.js
@@ -1,16 +1,53 @@
 import rootReducer from './reducers'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import createLogger from 'redux-logger'
 
-let store = createStore(rootReducer)
+const loggerMiddleware = createLogger()
 
-import { selectSubreddit, recievePosts } from './actions'
+// const store = createStore(rootReducer)
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware, // lets us dispatch() functions
+    loggerMiddleware // neat middleware that logs actions
+  )
+)
+
+import { selectSubreddit, fetchPosts } from './actions'
 store.dispatch(selectSubreddit('redux'))
 console.log(store.getState())
-const d = {
-  data:{
-    children: [{data:"c"}]
-  }
-}
-store.dispatch(recievePosts('redux', d))
-console.log(store.getState())
+store.dispatch(selectSubreddit('reactjs'))
+store.dispatch(fetchPosts('reactjs')).then(() => {
+  console.log("Finished fetch")
+  console.log(store.getState())
+})
 
+// const d = {
+//   data:{
+//     children: [{data:"c"}]
+//   }
+// }
+// store.dispatch(recievePosts('redux', d))
+// console.log(store.getState())
+
+// import thunkMiddleware from 'redux-thunk'
+// import createLogger from 'redux-logger'
+// import { createStore, applyMiddleware } from 'redux'
+// import { selectSubreddit, fetchPosts } from './actions'
+// import rootReducer from './reducers'
+//
+// const loggerMiddleware = createLogger()
+//
+// const store = createStore(
+//   rootReducer,
+//   applyMiddleware(
+//     thunkMiddleware, // lets us dispatch() functions
+//     loggerMiddleware // neat middleware that logs actions
+//   )
+// )
+//
+// store.dispatch(selectSubreddit('reactjs'))
+// store.dispatch(fetchPosts('reactjs')).then(() =>
+//   console.log(store.getState())
+// )
