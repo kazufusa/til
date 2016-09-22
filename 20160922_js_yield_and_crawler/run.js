580dab8a162c60b788b696d9506fedd4f0c47d26
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2016-09-22 12:28:34 +0900

    [co and Crawler] github crawler

--- 20160922_js_yield_and_crawler/run.js
+++ 20160922_js_yield_and_crawler/run.js
@@ -1,26 +1,51 @@
 const co = require('co')
 const client = require('cheerio-httpcli')
 
-const yahoo = 'http://www.yahoo.co.jp'
-const targets = [yahoo]
+const folders = ['https://github.com/ktty1220/cheerio-httpcli']
+const files = []
 
-function scrape(url){
+function scrapeFolder(url){
   return client.fetch(url)
   .then(function(result){
-    console.log(result.$('title').text())
-    targets.push(yahoo)
+    const $ = result.$
+    $('.file-wrap .js-navigation-open').each(function(){
+      if ($(this).url().indexOf('tree') >= 0 && $(this).text() !== '..'){
+        folders.push($(this).url())
+        console.log($(this).url())
+      } else if ($(this).url().indexOf('blob') >= 0){
+        files.push($(this).url())
+      }
+    })
   })
 }
 
-client.fetch(yahoo)
-.then(function(result){console.log('# preprocess')})
+function scrapeFile(url){
+  return client.fetch(url)
+  .then(function(result){
+    console.log(result.$.documentInfo().url)
+    // do something
+  })
+}
+
+(function(){
+  return new Promise(function(resolve){
+    console.log('# crawl folder tree')
+    resolve()
+  })
+})()
 .then(function(){
   return co(function *(){
-    let n = 0
-    while (targets.length > 0 && n < 10) {
-      yield scrape(targets.pop())
-      n ++
+    while (folders.length > 0) {
+      yield scrapeFolder(folders.pop())
     }
   })
 })
-.then(function(result){console.log('# postprocess')})
+.then(function(result){console.log('# scrape files')})
+.then(function(){
+  return co(function *(){
+    while (files.length > 0) {
+      yield scrapeFile(files.pop())
+    }
+  })
+})
+.then(function(result){console.log('# process end')})
