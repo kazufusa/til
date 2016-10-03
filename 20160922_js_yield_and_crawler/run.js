7d33778b6ff75bb07cccc705977f4261a1c51afa
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2016-10-03 22:14:33 +0900

    [co and Crawler] use arrow function

--- 20160922_js_yield_and_crawler/run.js
+++ 20160922_js_yield_and_crawler/run.js
@@ -1,41 +1,45 @@
 const co = require('co')
 const client = require('cheerio-httpcli')
+const fs = require('fs')
 
 const folders = ['https://github.com/ktty1220/cheerio-httpcli']
 const files = []
 const parallel = 1
 
-function scrapeFolder(url){
+global.files = []
+
+scrapeFolder = (url) =>{
   return client.fetch(url)
-  .then(function(result){
+  .then((result) => {
     const $ = result.$
-    $('.file-wrap .js-navigation-open').each(function(){
-      if ($(this).url().indexOf('tree') >= 0 && $(this).text() !== '..'){
-        folders.push($(this).url())
-        console.log($(this).url())
-      } else if ($(this).url().indexOf('blob') >= 0){
-        files.push($(this).url())
+    $('.file-wrap .js-navigation-open').each((_, item) => {
+      if ($(item).url().indexOf('tree') >= 0 && $(item).text() !== '..'){
+        folders.push($(item).url())
+        console.log($(item).url())
+      } else if ($(item).url().indexOf('blob') >= 0){
+        files.push($(item).url())
       }
     })
   })
 }
 
-function scrapeFile(url){
+scrapeFile = (url) => {
   return client.fetch(url)
-  .then(function(result){
+  .then((result) => {
     console.log(result.$.documentInfo().url)
     // do something
+    global.files.push(result.$.documentInfo().url)
   })
 }
 
-(function(){
-  return new Promise(function(resolve){
+(() => {
+  return new Promise((resolve) => {
     console.log('# crawl folder tree')
     resolve()
   })
 })()
-.then(function(){
-  return Promise.all(Array(parallel, null).map(function(){
+.then(() => {
+  return Promise.all(Array(parallel, null).map(() => {
     return co(function *(){
       while (folders.length > 0) {
         yield scrapeFolder(folders.pop())
@@ -43,9 +47,9 @@ function scrapeFile(url){
     })
   }))
 })
-.then(function(result){console.log('# scrape files')})
-.then(function(){
-  return Promise.all(Array(parallel, null).map(function(){
+.then((result) => {console.log('# scrape files')})
+.then(() => {
+  return Promise.all(Array(parallel, null).map(() => {
     return co(function *(){
       while (files.length > 0) {
         yield scrapeFile(files.pop())
@@ -53,4 +57,7 @@ function scrapeFile(url){
     })
   }))
 })
-.then(function(result){console.log('# process end')})
+.then((result) => {console.log('# process end')})
+.then((result) => {
+  fs.writeFileSync("public/index.json", JSON.stringify(global.files))
+})
