85412d5362d97199cb9fab010610d4230b4779ae
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2017-05-31 15:09:24 +0900

    [Bayes and Gibbs Sampling] update

--- 20170530_bayes_gibbs_sampling_01/run.r
+++ 20170530_bayes_gibbs_sampling_01/run.r
@@ -12,11 +12,7 @@ data   <- list(N=N, Y=Y)
 
 write.table(Y, file="points.csv", sep=",", row.names=F, col.names=F)
 
-iter   <- 11000
-warmup <- 1000
-seed   <- 123
-
-model2a <- "
+model2 <- "
 data {
   int<lower=1> N;
   vector[N] Y;
@@ -40,40 +36,14 @@ generated quantities {
 }
 "
 
-model2b <- "
-data {
-  int<lower=1> N;
-  vector[N] Y;
-}
-
-parameters {
-  real mu;
-  real<lower=0> s;
-}
-
-model {
-  for(n in 1:N)
-    target += 1/log(N) * normal_lpdf(Y[n] | mu, s);
-}
-
-generated quantities {
-  vector[N] log_likelihood;
-  for(n in 1:N)
-    log_likelihood[n] = normal_lpdf(Y[n] | mu, s);
-}
-"
-
 sink(file="/dev/null")
 suppressMessages({
   library(rstan)
-  fit2a <- stan(model_code=model2a, data=data, iter=iter, warmup=warmup, seed=seed, cores=4)
-  fit2b <- stan(model_code=model2b, data=data, iter=iter, warmup=warmup, seed=seed, cores=4)
+  fit2 <- stan(model_code=model2, data=data, iter=11000, warmup=1000, seed=123)
 })
 sink()
-cat("model 2a\n")
-print(summary(fit2a)$summary[c("mu", "s"), ])
-cat("\nmodel 2b\n")
-print(summary(fit2b)$summary[c("mu", "s"), ])
+cat("# results of the single normal distribution model with Rstan\n")
+print(summary(fit2)$summary[c("mu", "s"), ])
 
 waic <- function(log_likelihood) {
   training_error <- - mean(log(colMeans(exp(log_likelihood))))
@@ -89,9 +59,9 @@ wbic <- function(log_likelihood){
 
 ret <- matrix(0, 1, 2)
 colnames(ret) <- c('WAIC', 'WBIC')
-rownames(ret) <- c('model2')
+rownames(ret) <- c('single normal dist. model')
 
-ret[1,1] <- waic(extract(fit2a)$log_likelihood)
-ret[1,2] <- wbic(extract(fit2b)$log_likelihood)
+ret[1,1] <- waic(extract(fit2)$log_likelihood)
+ret[1,2] <- wbic(extract(fit2)$log_likelihood)
 cat("\n")
 ret
