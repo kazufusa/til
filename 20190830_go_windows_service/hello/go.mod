0a5d0d391e8afccff8810592b4f1b85b8f4e7f65
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2019-08-31 23:30:58 +0900

    [Go Service] initial commit

---
+++ 20190830_go_windows_service/hello/go.mod
@@ -0,0 +1,10 @@
+module my/command/app
+
+go 1.12
+
+require (
+	github.com/kardianos/service v1.0.0
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.4.0
+)
---
+++ 20190830_go_windows_service/hello/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/kardianos/service"
+)
+
+var logger service.Logger
+
+type exarvice struct {
+	exit chan struct{}
+}
+
+func (e *exarvice) run() error {
+
+	logger.Info("Exarvice Start !!!")
+
+	ticker := time.NewTicker(1 * time.Second)
+	for {
+		select {
+		case tm := <-ticker.C:
+			logger.Infof("Still running at %v", tm)
+		case <-e.exit:
+			ticker.Stop()
+			logger.Info("Exarvice Stop ...")
+			return nil
+		}
+	}
+}
+
+func (e *exarvice) Start(s service.Service) error {
+	if service.Interactive() {
+		logger.Info("Running in terminal.")
+	} else {
+		logger.Info("Running under service manager.")
+	}
+	e.exit = make(chan struct{})
+
+	go e.run()
+	return nil
+}
+
+func (e *exarvice) Stop(s service.Service) error {
+	close(e.exit)
+	return nil
+}
+
+func main() {
+	svcConfig := &service.Config{
+		Name:        "WebApp",
+		DisplayName: "WebApp (Go Service Example)",
+		Description: "WebApp service with GoLang",
+	}
+
+	// Create Exarvice service
+	program := &exarvice{}
+	s, err := service.New(program, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Setup the logger
+	errs := make(chan error, 5)
+	logger, err = s.Logger(errs)
+	if err != nil {
+		log.Fatal()
+	}
+
+	if len(os.Args) > 1 {
+
+		err = service.Control(s, os.Args[1])
+		if err != nil {
+			fmt.Printf("Failed (%s) : %s\n", os.Args[1], err)
+			return
+		}
+		fmt.Printf("Succeeded (%s)\n", os.Args[1])
+		return
+	}
+
+	// run in terminal
+	s.Run()
+}
