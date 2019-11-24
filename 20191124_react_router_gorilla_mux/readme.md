5296d6487bbf09d61c60e24f1f9e82d6aa0e2ac8
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2019-11-24 15:27:38 +0900

    [ReactRouter and Gorilla/mux] initial commit

---
+++ 20191124_react_router_gorilla_mux/readme.md
@@ -0,0 +1,14 @@
+# React-Router and Gorilla/mux
+
+```
+$ cd client
+$ yarn && yarn build
+$ cd ../server
+$ go run main.go &
+$ curl localhost:8000/api/health
+{"ok":true}
+$ curl localhost:8000
+<!doctype html><html lang="en"><head><meta charset="utf-8"/><link rel="icon" href="/favicon.ico"/><....
+$ curl localhost:8000/about
+<!doctype html><html lang="en"><head><meta charset="utf-8"/><link rel="icon" href="/favicon.ico"/><....
+```
---
+++ 20191124_react_router_gorilla_mux/server/main.go
@@ -0,0 +1,61 @@
+package main
+
+// acknowledgement: https://github.com/gorilla/mux#serving-single-page-applications
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	path = filepath.Join(h.staticPath, path)
+
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	})
+
+	spa := spaHandler{staticPath: "../client/build", indexPath: "index.html"}
+	router.PathPrefix("/").Handler(spa)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    "127.0.0.1:8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
