9c7c745ae595102efc89bc8426ba7dacc8ec0e69
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2022-01-12 09:20:17 +0900

    feat(algorithm and data structure): add binary_search_2.c

---
+++ 20220109_book_algorithm_and_data_structure/02_search/binary_search_2.c
@@ -0,0 +1,46 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+
+#define NOT_FOUND (-1)
+#define N (2)
+
+int binary_search_2(int x, int a[], int n) {
+  int left, right, mid;
+  left = 0;
+  right = n - 1;
+  while (left < right) {
+    mid = (left + right) / 2;
+    if (a[mid] < x) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  if (a[left] == x){
+    return left;
+  };
+
+  return NOT_FOUND;
+}
+
+int main() {
+  int i, r, array[N];
+  srand((unsigned int)time(NULL));
+
+  printf("0 %3d\n", array[0] = rand() % 3);
+  for (i = 1; i < N; i++) {
+    printf("%d %3d\n", i, array[i] = array[i - 1] + rand() % 3);
+  }
+  printf("何を探しますか?:");
+  scanf("%d", &i);
+
+  r = binary_search_2(i, array, N);
+  if (r == NOT_FOUND) {
+    printf("%dは見つかりません\n", i);
+  } else {
+    printf("%dは%d番目です", i, r);
+  }
+
+  return EXIT_SUCCESS;
+}
