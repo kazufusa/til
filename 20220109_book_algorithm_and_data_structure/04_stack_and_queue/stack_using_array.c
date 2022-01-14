2cb833cef6356014fb16cde33490c1f98a90759c
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2022-01-14 23:22:11 +0900

    feat(algorithm and data structure): add stack with array

--- 20220109_book_algorithm_and_data_structure/04_stack_and_queue/stack_using_array.c
+++ 20220109_book_algorithm_and_data_structure/04_stack_and_queue/stack_using_array.c
@@ -2,23 +2,41 @@
 #include "stdlib.h"
 
 #define N (10)
+#define SUCCESS (0)
+#define FAILURE (-1)
 
 int stack[N];
 int stackpoint;
 
+int push(int n) {
+  if (stackpoint == N - 1) {
+    return FAILURE;
+  }
+  stack[++stackpoint] = n;
+  return SUCCESS;
+}
+
+int pop(int *n) {
+  if (stackpoint == -1) {
+    return FAILURE;
+  }
+  *n = stack[stackpoint--];
+  return SUCCESS;
+}
+
 int main() {
-  int i;
+  int i, r;
   stackpoint = -1;
 
-  // add value to stack
-  while (stackpoint < N - 1) {
-    stackpoint++;
-    stack[stackpoint] = stackpoint;
-  }
+  do {
+    printf("Please input value to push, and end with 0:");
+    scanf("%d", &i);
+  } while (i != 0 && push(i) == SUCCESS);
 
   // pop values from stack
-  for (; stackpoint >= 0; stackpoint--) {
-    printf("%d \n", stack[stackpoint]);
+  printf("pop all values from stack\n");
+  while (pop(&i) == SUCCESS) {
+    printf("%d ", i);
   }
   return EXIT_SUCCESS;
 }
