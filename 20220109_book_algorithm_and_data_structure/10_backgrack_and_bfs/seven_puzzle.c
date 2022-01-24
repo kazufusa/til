18f7010dac96676e20f37f62bb651e9d049155fb
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2022-01-24 16:30:27 +0900

    feat(algorithm and data structure): add seven_puzzle.c

--- 20220109_book_algorithm_and_data_structure/10_backgrack_and_bfs/seven_puzzle.c
+++ 20220109_book_algorithm_and_data_structure/10_backgrack_and_bfs/seven_puzzle.c
@@ -10,7 +10,7 @@ typedef struct tagPATTERN {
   u_int32_t prev_hash;
 } PATTERN;
 
-PATTERN *queue[2048];
+PATTERN *queue[100000];
 int ipush = 0;
 int ipop = 0;
 
@@ -19,6 +19,17 @@ void push(u_int32_t hash, u_int32_t prev_hash) {
   queue[ipush]->hash = hash;
   queue[ipush]->prev_hash = prev_hash;
   ipush++;
+}
+
+PATTERN *find(u_int32_t v) {
+  int i = 0;
+  while (queue[i] != NULL) {
+    if (queue[i]->hash == v) {
+      return queue[i];
+    }
+    i++;
+  }
+  return NULL;
 }
 
 PATTERN *pop() { return queue[ipop++]; }
@@ -45,6 +56,19 @@ void bprint(u_int32_t v) {
   printf("\n");
 };
 
+void print(u_int32_t v) {
+  printf("┌───┬───┬───┬───┐\n│");
+  for (int i = 7; i >= 4; i--) {
+    printf(" %d │", (v &  15 <<(   4*i)) >>  ( 4*i));
+  }
+  printf("\n├───┼───┼───┼───┤\n│");
+  for (int i = 3; i >= 0; i--) {
+    printf(" %d │", (v &  15 <<(   4*i)) >>  ( 4*i));
+  }
+  printf("\n");
+  printf("└───┴───┴───┴───┘\n\n");
+}
+
 u_int32_t blankPos(u_int32_t hash) {
   int i;
   for (i = 0; i < 8; i++) {
@@ -55,17 +79,27 @@ u_int32_t blankPos(u_int32_t hash) {
   return 0xfffffff;
 }
 
-u_int32_t moveV(u_int32_t hash) {
+u_int32_t moveUp(u_int32_t hash) {
   int bp = blankPos(hash);
   int to = bp + 4;
   int mask = 15 << (4 * to);
-  if (7 < to) {
+  if (3 < bp) {
     return 0xffffffff;
   }
-  return (hash | ((hash & mask) >> (4 * (bp - to)))) & ~(15 << (4 * to));
+  return (hash | ((hash & mask) >> (4 * 4))) & ~(15 << (4 * to));
 }
 
-u_int32_t moveR(u_int32_t hash) {
+u_int32_t moveDown(u_int32_t hash) {
+  int bp = blankPos(hash);
+  int to = bp + 4;
+  int mask = 15 << (4 * to);
+  if (bp < 4) {
+    return 0xffffffff;
+  }
+  return (hash | (hash & mask) << (4 * 4)) & ~(15 << (4 * to));
+}
+
+u_int32_t moveLeft(u_int32_t hash) {
   int bp = blankPos(hash);
   int to = bp + 1;
   int mask = 15 << (4 * to);
@@ -75,7 +109,7 @@ u_int32_t moveR(u_int32_t hash) {
   return (hash | ((hash & mask) >> (4))) & ~(15 << (4 * to));
 }
 
-u_int32_t moveL(u_int32_t hash) {
+u_int32_t moveRight(u_int32_t hash) {
   int bp = blankPos(hash);
   int to = bp - 1;
   int mask = 15 << (4 * to);
@@ -85,19 +119,66 @@ u_int32_t moveL(u_int32_t hash) {
   return (hash | ((hash & mask) << (4))) & ~(15 << (4 * to));
 }
 
+PATTERN *solve(u_int32_t start, u_int32_t goal) {
+  u_int32_t new_pattern;
+  PATTERN *pattern;
+  ipush = ipop = 0;
+  push(start, 0xffffffff);
+  while (1) {
+    pattern = pop();
+    if (pattern == NULL) {
+      return NULL;
+    } else if (pattern->hash == goal) {
+      return pattern;
+    }
+    new_pattern = moveDown(pattern->hash);
+    if (new_pattern != 0xffffffff && find(new_pattern) == NULL) {
+      push(new_pattern, pattern->hash);
+    }
+    new_pattern = moveUp(pattern->hash);
+    if (new_pattern != 0xffffffff && find(new_pattern) == NULL) {
+      push(new_pattern, pattern->hash);
+    }
+    new_pattern = moveRight(pattern->hash);
+    if (new_pattern != 0xffffffff && find(new_pattern) == NULL) {
+      push(new_pattern, pattern->hash);
+    }
+    new_pattern = moveLeft(pattern->hash);
+    if (new_pattern != 0xffffffff && find(new_pattern) == NULL) {
+      push(new_pattern, pattern->hash);
+    }
+  }
+}
+
 int main() {
   u_int32_t start, goal;
+  int i = 0, n = 10;
+  PATTERN *ans;
+  int *route;
   goal = 0x12345670;
-  start = (2 << (4 * 7)) + (7 << (4 * 6)) + (1 << (4 * 5)) + (4 << (4 * 4)) +
-          (5 << (4 * 3)) + (0 << (4 * 2)) + (2 << (4 * 1)) + (6 << (4 * 0));
+  start = 0x27145036;
+  for (int i = 0; i < 2048; i++) {
+    queue[i] = NULL;
+  }
 
-  bprint(start);
-  printf("%d\n", blankPos(start));
-  bprint(moveL(start));
-  bprint(moveR(start));
-
-  bprint(goal);
-  printf("%d\n", blankPos(goal));
+  ans = solve(start, goal);
+  if (ans == NULL) {
+    printf("答えが見つかりませんでした.\n");
+    return EXIT_FAILURE;
+  }
+  route = (int *)malloc(sizeof(int) * n);
+  while (ans->hash != start) {
+    route[i++] = ans->hash;
+    ans = find(ans->prev_hash);
+    if (n <= i) {
+      n += 10;
+      route = (int *)realloc(route, sizeof(int) * n);
+    }
+  }
+  route[i] = start;
+  for (; i >= 0; i--) {
+    print(route[i]);
+  }
 
   cleanup();
   return EXIT_SUCCESS;
