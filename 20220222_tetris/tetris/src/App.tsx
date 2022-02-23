66c681c52a71b6c8a93f04d9a8ea91747cef6f15
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2022-02-23 23:54:11 +0900

    feat(tetris.js): WIP

--- 20220222_tetris/tetris/src/App.tsx
+++ 20220222_tetris/tetris/src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
-
 
 enum Rotate { Right, Left }
 
@@ -20,7 +18,7 @@ type Tetromino = [
   [TetrominoLabel, TetrominoLabel, TetrominoLabel, TetrominoLabel],
 ];
 
-const RotateRight = (t: Tetromino): Tetromino => {
+const RotateTetriminoToRight = (t: Tetromino): Tetromino => {
   return [
     [t[3][0], t[2][0], t[1][0], t[0][0]],
     [t[3][1], t[2][1], t[1][1], t[0][1]],
@@ -29,26 +27,42 @@ const RotateRight = (t: Tetromino): Tetromino => {
   ]
 }
 
+const RotateTetriminoToLeft = (t: Tetromino): Tetromino => {
+  return [
+    [t[0][3], t[1][3], t[2][3], t[3][3]],
+    [t[0][2], t[1][2], t[2][2], t[3][2]],
+    [t[0][1], t[1][1], t[2][1], t[3][1]],
+    [t[0][0], t[1][0], t[2][0], t[3][0]],
+  ]
+}
+
+const RotateRight = (t: Tetris): Tetris => (
+  { ...t, Tetrimino: RotateTetriminoToRight(t.Tetrimino) }
+)
+
+const RotateLeft = (t: Tetris): Tetris => (
+  { ...t, Tetrimino: RotateTetriminoToLeft(t.Tetrimino) }
+)
+
 const InitialTetromino: Record<TetrominoName, Tetromino> = {
-  [TetrominoName.I]: [[-1, -1, -1, -1], [-1, -1, -1, -1], [0, 0, 0, 0], [-1, -1, -1, -1]],
+  [TetrominoName.I]: [[-1, -1, -1, -1], [0, 0, 0, 0], [-1, -1, -1, -1], [-1, -1, -1, -1]],
   [TetrominoName.O]: [[-1, -1, -1, -1], [-1, 1, 1, -1], [-1, 1, 1, -1], [-1, -1, -1, -1]],
   [TetrominoName.S]: [[-1, -1, -1, -1], [-1, 2, 2, -1], [2, 2, -1, -1], [-1, -1, -1, -1]],
   [TetrominoName.Z]: [[-1, -1, -1, -1], [3, 3, -1, -1], [-1, 3, 3, -1], [-1, -1, -1, -1]],
-  [TetrominoName.J]: [[-1, -1, -1, -1], [4, -1, -1, -1], [4, 4, 4, -1], [-1, -1, -1, -1]],
-  [TetrominoName.L]: [[-1, -1, -1, -1], [-1, -1, 5, -1], [5, 5, 5, -1], [-1, -1, -1, -1]],
-  [TetrominoName.T]: [[-1, -1, -1, -1], [-1, 6, -1, -1], [6, 6, 6, -1], [-1, -1, -1, -1]],
+  [TetrominoName.J]: [[-1, -1, -1, -1], [4, 4, 4, -1], [-1, -1, 4, -1], [-1, -1, -1, -1]],
+  [TetrominoName.L]: [[-1, -1, -1, -1], [5, 5, 5, -1], [5, -1, -1, -1], [-1, -1, -1, -1]],
+  [TetrominoName.T]: [[-1, -1, -1, -1], [6, 6, 6, -1], [-1, 6, -1, -1], [-1, -1, -1, -1]],
 }
 
 const RandomTetromino = (): TetrominoName => Math.floor(Math.random() * 7);
 
 type Tetris = {
   Board: number[][];
-  Tetrimino: number[][];
-  TetriminoX: number,
-  TetriminoY: number,
-  TetriminoRotation: 0 | 1 | 2 | 3,
   GameOver: boolean;
   NextTetrimino: TetrominoName;
+  Tetrimino: Tetromino;
+  TetriminoX: number,
+  TetriminoY: number,
 };
 
 const TetriminoIndex = (board: Tetris): (x: number, y: number) => [number | undefined, number | undefined] => (x: number, y: number) => {
@@ -61,10 +75,9 @@ const TetriminoIndex = (board: Tetris): (x: number, y: number) => [number | unde
 
 const InitialBoard: Tetris = {
   Board: [...Array(20)].map(() => [...Array(10)].map(() => Empty)),
-  Tetrimino: RotateRight(InitialTetromino[1]),
+  Tetrimino: InitialTetromino[RandomTetromino()],
   TetriminoX: 3,
   TetriminoY: 1,
-  TetriminoRotation: 0,
   GameOver: false,
   NextTetrimino: RandomTetromino(),
 };
@@ -97,12 +110,27 @@ const Board: React.FC<Tetris> = (tetris) => {
 };
 
 const App: React.FC = () => {
+  const [tetris, setTetris] = useState<Tetris>(InitialBoard);
+  useEffect(() => {
+    window.addEventListener("keydown", (event) => {
+      switch (event.code) {
+        case "ArrowRight":
+          setTetris(RotateRight)
+          break
+        case "ArrowLeft":
+          setTetris(RotateLeft)
+          break
+        default:
+          break
+      }
+    })
+  }, [])
 
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello Vite + React!</p>
-        <Board {...InitialBoard} />
+        <Board {...tetris} />
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
