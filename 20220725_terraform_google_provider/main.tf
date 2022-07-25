eb9e4bdb87d27151fdc488f26c0a9da500ec0910
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2022-07-25 23:52:06 +0900

    feat(terraform): google provider

---
+++ 20220725_terraform_google_provider/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.29.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  # project information
+  # joint information
+  # region information
+}
