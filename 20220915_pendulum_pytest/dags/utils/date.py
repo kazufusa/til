6ceca46d9a4c701774fa7fa8be75b7feb5a07d17
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2022-09-18 22:03:26 +0900

    refactor(pendulum,pytest): fix code

--- 20220915_pendulum_pytest/dags/utils/date.py
+++ 20220915_pendulum_pytest/dags/utils/date.py
@@ -1,12 +1,7 @@
-import pendulum
 from pendulum import DateTime
 
 
-def last_range(_dt: DateTime) -> str:
-    dt = _dt.in_tz(pendulum.UTC)
-    if dt.day_of_week < pendulum.WEDNESDAY:
-        # pendulumのデフォルトでは週は月曜日始まり, `pendulum._WEEK_STARTS_AT`
-        st = dt.subtract(days=2).start_of("week")
-    else:
-        st = dt.start_of("week")
+def last_range(dt: DateTime) -> str:
+    # pendulumのデフォルトでは週は月曜日始まり, `pendulum._WEEK_STARTS_AT`
+    st = dt.in_tz("Asia/Tokyo").subtract(days=2).start_of("week")
     return f"{st.strftime('%Y-%m-%d')} AND {st.add(days=1).strftime('%Y-%m-%d')}"
--- 20220915_pendulum_pytest/tests/dags/utils/test_date.py
+++ 20220915_pendulum_pytest/tests/dags/utils/test_date.py
@@ -4,10 +4,10 @@ from dags.utils.date import last_range
 
 
 def test_last_range():
-    assert last_range(datetime(2022, 9, 18, 17, 0, 0, 0, UTC)) == "2022-09-12 AND 2022-09-13", "日曜日"
-    assert last_range(datetime(2022, 9, 19, 17, 0, 0, 0, UTC)) == "2022-09-12 AND 2022-09-13", "月曜日"
-    assert last_range(datetime(2022, 9, 20, 17, 0, 0, 0, UTC)) == "2022-09-12 AND 2022-09-13", "火曜日"
-    assert last_range(datetime(2022, 9, 21, 17, 0, 0, 0, UTC)) == "2022-09-19 AND 2022-09-20", "水曜日"
-    assert last_range(datetime(2022, 9, 22, 17, 0, 0, 0, UTC)) == "2022-09-19 AND 2022-09-20", "木曜日"
-    assert last_range(datetime(2022, 9, 23, 17, 0, 0, 0, UTC)) == "2022-09-19 AND 2022-09-20", "金曜日"
-    assert last_range(datetime(2022, 9, 24, 17, 0, 0, 0, UTC)) == "2022-09-19 AND 2022-09-20", "土曜日"
+    assert last_range(datetime(2022, 9, 17, 17, 0, 0, 0, UTC)) == "2022-09-12 AND 2022-09-13", "日本時間日曜日"
+    assert last_range(datetime(2022, 9, 18, 17, 0, 0, 0, UTC)) == "2022-09-12 AND 2022-09-13", "日本時間月曜日"
+    assert last_range(datetime(2022, 9, 19, 17, 0, 0, 0, UTC)) == "2022-09-12 AND 2022-09-13", "日本時間火曜日"
+    assert last_range(datetime(2022, 9, 20, 17, 0, 0, 0, UTC)) == "2022-09-19 AND 2022-09-20", "日本時間水曜日"
+    assert last_range(datetime(2022, 9, 21, 17, 0, 0, 0, UTC)) == "2022-09-19 AND 2022-09-20", "日本時間木曜日"
+    assert last_range(datetime(2022, 9, 22, 17, 0, 0, 0, UTC)) == "2022-09-19 AND 2022-09-20", "日本時間金曜日"
+    assert last_range(datetime(2022, 9, 23, 17, 0, 0, 0, UTC)) == "2022-09-19 AND 2022-09-20", "日本時間土曜日"
