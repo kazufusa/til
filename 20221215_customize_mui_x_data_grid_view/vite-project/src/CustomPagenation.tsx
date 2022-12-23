f7a504eef5aa6029894606035d978eaa023a34c3
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2022-12-23 22:15:13 +0900

    feat(mui,grid): original scroll tool, WIP

--- 20221215_customize_mui_x_data_grid_view/vite-project/src/CustomPagenation.tsx
+++ 20221215_customize_mui_x_data_grid_view/vite-project/src/CustomPagenation.tsx
@@ -3,6 +3,7 @@ import {
   gridPageSelector,
   useGridApiContext,
   useGridSelector,
+  useGridScrollFn,
   gridColumnsTotalWidthSelector,
 } from '@mui/x-data-grid';
 import Pagination from '@mui/material/Pagination';
@@ -13,9 +14,12 @@ export default function CustomPagination() {
   const page = useGridSelector(apiRef, gridPageSelector);
   const pageCount = useGridSelector(apiRef, gridPageCountSelector);
   console.log(gridColumnsTotalWidthSelector(apiRef))
+  console.log(apiRef.current?.getScrollPosition().left)
+  console.log(apiRef.current?.getRootDimensions());
 
   return (
     <>
+      top: {apiRef.current?.getScrollPosition().top} / left: {apiRef.current?.getScrollPosition().left}
       <Pagination
         color="primary"
         count={pageCount}
@@ -23,14 +27,8 @@ export default function CustomPagination() {
         onChange={(_: React.ChangeEvent<unknown>, value) => apiRef.current.setPage(value - 1)}
       />
       {pageCount !== 0 && `${page + 1} / ${pageCount}`}
-      <Button onClick={() => apiRef?.current?.scroll({ left: 500 })}> move to pos</Button>
-      <Button onClick={() => { apiRef?.current?.scrollToIndexes({ colIndex: 0 }); console.log(apiRef?.current?.getScrollPosition()) }}> move to column</Button>
-      <Button onClick={() => { apiRef?.current?.scrollToIndexes({ colIndex: 1 }); console.log(apiRef?.current?.getScrollPosition()) }}> move to column</Button>
-      <Button onClick={() => { apiRef?.current?.scrollToIndexes({ colIndex: 2 }); console.log(apiRef?.current?.getScrollPosition()) }}> move to column</Button>
-      <Button onClick={() => { apiRef?.current?.scrollToIndexes({ colIndex: 3 }); console.log(apiRef?.current?.getScrollPosition()) }}> move to column</Button>
-      <Button onClick={() => { apiRef?.current?.scrollToIndexes({ colIndex: 4 }); console.log(apiRef?.current?.getScrollPosition()) }}> move to column</Button>
-      <Button onClick={() => { apiRef?.current?.scrollToIndexes({ colIndex: 5 }); console.log(apiRef?.current?.getScrollPosition()) }}> move to column</Button>
-      <Button onClick={() => { apiRef?.current?.scrollToIndexes({ colIndex: 6 }); console.log(apiRef?.current?.getScrollPosition()) }}> move to column</Button>
+      <Button onClick={() => apiRef?.current?.scroll({ left: 100 })}> move to pos</Button>
+      <Button onClick={() => { apiRef?.current?.scrollToIndexes({ colIndex: 6 }) }}> move to column</Button>
     </>
   );
 }
--- 20221215_customize_mui_x_data_grid_view/vite-project/src/Table.tsx
+++ 20221215_customize_mui_x_data_grid_view/vite-project/src/Table.tsx
@@ -6,12 +6,13 @@ export default function Table() {
   const { data } = useDemoData({
     dataSet: 'Employee',
     rowLength: 30,
-    maxColumns: 3,
+    maxColumns: 8,
   });
 
   return (
     <div style={{ height: 400, width: '100%' }}>
       <DataGrid
+        scrollbarSize={100}
         components={{
           Toolbar: () => <>
             <h1>Title</h1>
