fe2483c3b23eb5961d1812f5f124d01f195c7719
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2023-07-08 22:44:50 +0900

    feat: 🎸 react image size: enable multiple files

--- 20230707_react_file_upload/vite-project/src/App.tsx
+++ 20230707_react_file_upload/vite-project/src/App.tsx
@@ -1,37 +1,38 @@
 import { useState, useRef, useEffect } from "react";
 import "./App.css";
 
-
-type ImageFIle = {
+type ImageFile = {
   file: File;
   imagePreviewUrl: string | null;
-}
+  width?: number;
+  height?: number;
+};
 
 function App() {
-  const [file, setFile] = useState<ImageFIle | null>(null);
+  const [file, setFile] = useState<ImageFile | null>(null);
   const [size, setSize] = useState<string>("");
   const ref = useRef<HTMLImageElement>(null);
   const onChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0]
+    const file = e.target.files?.[0];
     if (file) {
-      var reader = new FileReader()
+      var reader = new FileReader();
       reader.onload = () => {
-        if (typeof reader.result === 'string' || reader.result === null) {
-          setFile({ file, imagePreviewUrl: reader.result })
+        if (typeof reader.result === "string" || reader.result === null) {
+          setFile({ file, imagePreviewUrl: reader.result });
         }
-      }
+      };
       reader.readAsDataURL(file);
     } else {
-      setFile(null)
+      setFile(null);
     }
   };
 
   const handleImageLoad = () => {
-    setSize(`${ref.current?.naturalWidth} x ${ref.current?.naturalHeight}`)
+    setSize(`${ref.current?.naturalWidth} x ${ref.current?.naturalHeight}`);
   };
 
   useEffect(() => {
-    const element = ref.current
+    const element = ref.current;
     if (element) {
       element.addEventListener("load", handleImageLoad);
     }
@@ -52,15 +53,73 @@ function App() {
             accept="image/*"
             onChange={onChangeFile}
           />
-          <img ref={ref} src={file?.imagePreviewUrl ?? ""} style={{
-            width: "100px",
-            height: "100px",
-            objectFit: "contain",
-          }} />
+          <img
+            ref={ref}
+            src={file?.imagePreviewUrl ?? ""}
+            style={{
+              width: "100px",
+              height: "100px",
+              objectFit: "contain",
+            }}
+          />
           {size}
         </div>
       </div>
+      <ImageList />
     </>
+  );
+}
+
+function ImageList() {
+  const [files, setFiles] = useState<ImageFile[]>([]);
+  const onChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFiles([]);
+    [...(e.target.files ?? [])].map((file) => {
+      var reader = new FileReader();
+      reader.onload = () => {
+        const result = reader.result;
+        if (
+          (typeof result === "string" || result === null) &&
+          !files.find((f) => f.imagePreviewUrl === result)
+        ) {
+          setFiles((files) => [...files, { file, imagePreviewUrl: result }]);
+        }
+      };
+      reader.readAsDataURL(file);
+    });
+  };
+
+  const onLoadFile = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    const url = (e.target as HTMLImageElement).src;
+    const width = (e.target as HTMLImageElement).naturalWidth;
+    const height = (e.target as HTMLImageElement).naturalHeight;
+    setFiles((files) =>
+      files.map((file) =>
+        file.imagePreviewUrl === url ? { ...file, width, height } : { ...file }
+      )
+    );
+  };
+  return (
+    <div>
+      <input
+        name="file"
+        type="file"
+        accept="image/*"
+        multiple
+        onChange={onChangeFile}
+      />
+      <ul>
+        {files.map((file) => (
+          <li key={`${file.imagePreviewUrl}`} >
+            {file.width && file.height && `${file.width} x ${file.height}`}
+            <img
+              onLoad={onLoadFile}
+              src={file.imagePreviewUrl ?? ""}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
