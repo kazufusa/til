e60ad0333140ae48d88046594970c24541f6ab63
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2023-10-19 23:18:56 +0900

    feat: 🎸 grid css

---
+++ 20231019_css_grid/vite-project/src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="parent">
+        <div className="child" style={{ width: "50px" }} />
+        <div className="child" style={{ width: "200px" }} />
+        <div className="child" style={{ width: "150px" }} />
+        <div className="child" style={{ width: "50px" }} />
+        <div className="child" style={{ width: "50px" }} />
+        <div className="child" style={{ width: "200px" }} />
+        <div className="child" style={{ width: "200px" }} />
+        <div className="child" style={{ width: "150px" }} />
+        <div className="child" style={{ width: "150px" }} />
+        <div className="child" style={{ width: "50px" }} />
+      </div>
+    </>
+  )
+}
+
+export default App
