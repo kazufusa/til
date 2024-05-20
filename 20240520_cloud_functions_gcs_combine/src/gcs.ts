bdbb615c0fc3eb322f5ce86a0d666856a4ef334b
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2024-05-21 00:07:35 +0900

    feat: 🎸 cloud functions

---
+++ 20240520_cloud_functions_gcs_combine/src/gcs.ts
@@ -0,0 +1,56 @@
+import { Storage } from "@google-cloud/storage";
+import { chunk } from "./chunk";
+
+const COMBINE_LIMIT = 32;
+
+function ensureTrailingSlash(path: string): string {
+  return path.endsWith("/") ? path : `${path}/`;
+}
+
+async function listFiles(
+  storage: Storage,
+  bucketName: string,
+  prefix: string,
+): Promise<string[]> {
+  const [files] = await storage.bucket(bucketName).getFiles({ prefix });
+  return files.map((v) => v.name).filter((v) => !/\/_combined[^\/]*$/.test(v));
+}
+
+async function combineRecursive(
+  storage: Storage,
+  bucketName: string,
+  prefix: string,
+  files: string[],
+  combinedFileSuffix: string = "",
+): Promise<string> {
+  if (files.length === 0) return "";
+  if (files.length === 1) return files[0];
+  const bucket = storage.bucket(bucketName);
+  const destinationFileName = `${prefix}_combined${combinedFileSuffix}`;
+  const sources: string[] = files.length > COMBINE_LIMIT
+    ? await Promise.all(
+      chunk(files, COMBINE_LIMIT).map(async (v, i) =>
+        combineRecursive(
+          storage,
+          bucketName,
+          prefix,
+          v,
+          `${combinedFileSuffix}_${i}`,
+        )
+      ),
+    )
+    : files;
+  await bucket.combine(sources, destinationFileName);
+  return destinationFileName;
+}
+
+export async function combine(
+  projectId: string,
+  bucketName: string,
+  _prefix: string,
+): Promise<string> {
+  const prefix = ensureTrailingSlash(_prefix);
+  const storage: Storage = new Storage({ projectId });
+  const files = await listFiles(storage, bucketName, prefix);
+  return await combineRecursive(storage, bucketName, prefix, files);
+}
---
+++ 20240520_cloud_functions_gcs_combine/src/index.ts
@@ -0,0 +1,19 @@
+import * as functions from "@google-cloud/functions-framework";
+import { combine } from "./gcs";
+
+functions.http("sample", async (req, res) => {
+  const { projectId: _projectId, bucketName: _bucketName, pathPrefix: _pathPrefix } = req.query
+  if (!_projectId || !_bucketName || !_pathPrefix) {
+    return res.status(400).send({ error: "Missing required query parameters" });
+  }
+  const projectId = String(_projectId)
+  const bucketName = String(_bucketName)
+  const pathPrefix = String(_pathPrefix)
+  try {
+    const combinedFileName = await combine(projectId, bucketName, pathPrefix);
+    res.status(200).send({ result: combinedFileName });
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({ error: "Failed to combine files" });
+  }
+});
